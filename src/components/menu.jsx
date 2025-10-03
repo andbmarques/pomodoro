bcd2e048b17e72d936ac1c2db02b728bde3168b4
Anderson B. Marques <andbmarques@gmail.com>
2025-10-03 15:53:49 -0300

    Add Interval Timer integration

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -1,20 +1,46 @@
-import { Button, Container, Flex } from "@radix-ui/themes"
+import { Button, Container, Flex } from "@radix-ui/themes";
 
-const Menu = ({ focusTimer }) => {
-    return (
-        <Container>
-            <Flex direction="column" gap="2">
-                <Flex gap="2" justify="center">
-                    <Button className="button" color="red" disabled={focusTimer.isFocusRunning} variant="soft" onClick={focusTimer.focusStart}>Start</Button>
-                    <Button className="button" color="red" disabled={!focusTimer.isFocusRunning} variant="soft" onClick={focusTimer.focusReset}>Reset</Button>
-                    <Button className="button" color="red" disabled={!focusTimer.isFocusRunning} variant="soft" onClick={focusTimer.focusPause}>Pause</Button>
-                </Flex>
-                <Flex justify="center" px="2">
-                    <Button style={{ width: "100%" }} color="red" variant="outline">Config</Button>
-                </Flex>
-            </Flex>
-        </Container>
-    )
-}
+const Menu = ({ focusTimer, intervalTimer, actualTimer }) => {
+  return (
+    <Container>
+      <Flex direction="column" gap="2">
+        <Flex gap="2" justify="center">
+          <Button
+            className="button"
+            color="red"
+            disabled={focusTimer.isFocusRunning || intervalTimer.isIntervalRunning}
+            variant="soft"
+            onClick={actualTimer === "focus" ? focusTimer.focusStart : intervalTimer.intervalStart}
+          >
+            Start
+          </Button>
+          <Button
+            className="button"
+            color="red"
+            disabled={!focusTimer.isFocusRunning && !intervalTimer.isIntervalRunning}
+            variant="soft"
+            onClick={actualTimer === "focus" ? focusTimer.focusReset : intervalTimer.intervalReset}
+          >
+            Reset
+          </Button>
+          <Button
+            className="button"
+            color="red"
+            disabled={!focusTimer.isFocusRunning && !intervalTimer.isIntervalRunning}
+            variant="soft"
+            onClick={actualTimer === "focus" ? focusTimer.focusPause : intervalTimer.intervalPause}
+          >
+            Pause
+          </Button>
+        </Flex>
+        <Flex justify="center" px="2">
+          <Button style={{ width: "100%" }} color="red" variant="outline">
+            Config
+          </Button>
+        </Flex>
+      </Flex>
+    </Container>
+  );
+};
 
-export default Menu
+export default Menu;
